9d4bdc804d5483e7de997f191a5b5614ed9344b2
unknown <abidisofien1988@gmail.com>
2022-10-26 10:50:40 +0100

    corrected commit #04_4

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,12 +2,22 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
 import FacebookProvider from "next-auth/providers/facebook";
+import InstagramProvider from "next-auth/providers/instagram";
+
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "../../../lib/mongodb";
 
 export const authOptions = {
 	// Configure one or more authentication providers
 	providers: [
+		FacebookProvider({
+			clientId: process.env.FACEBOOK_CLIENT_ID,
+			clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+		}),
+		InstagramProvider({
+			clientId: process.env.INSTAGRAM_CLIENT_ID,
+			clientSecret: process.env.INSTAGRAM_CLIENT_SECRET,
+		}),
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -16,10 +26,7 @@ export const authOptions = {
 			clientId: process.env.GITHUB_ID,
 			clientSecret: process.env.GITHUB_SECRET,
 		}),
-		FacebookProvider({
-			clientId: process.env.FACEBOOK_CLIENT_ID,
-			clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-		}),
+
 		/* 		EmailProvider({
 			server: {
 				host: process.env.EMAIL_SERVER_HOST,
--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -2,13 +2,15 @@ import { getProviders, signIn } from "next-auth/react";
 import GoogleIcon from "@mui/icons-material/Google";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import FacebookIcon from "@mui/icons-material/Facebook";
+import EmailIcon from "@mui/icons-material/Email";
+import InstagramIcon from "@mui/icons-material/Instagram";
 import { Button } from "@mui/material";
 
 export default function SignIn({ providers }) {
 	return (
 		<div className="w-screen h-screen bg-gray-100 flex items-center justify-center">
-			<div className="flex flex-col w-4/5 h-1/2 text-white bg-gradient-to-tr from-black to-gray-500 shadow-2xl shadow-emerald-800 max-w-lg mx-auto rounded-lg items-center p-5">
-				<h1 className="font-bold underline mb-4">Sign In</h1>
+			<div className="flex flex-col w-4/5 min-h-fit text-white bg-gradient-to-tr from-black to-gray-500 shadow-2xl shadow-emerald-800 max-w-lg mx-auto rounded-lg items-center p-5 space-y-4">
+				<h1 className="font-bold underline mb-4">Sign In to WEAVERS</h1>
 				<img src="/images/nextauth.png" alt="auth" className="w-16 h-24" />
 				<div className="flex flex-col items-center justify-center w-full h-full space-y-4">
 					{Object.values(providers).map((provider) => (
@@ -16,12 +18,16 @@ export default function SignIn({ providers }) {
 							<Button
 								onClick={() => signIn(provider.id, { callbackUrl: "/" })}
 								startIcon={
-									provider.name === "Google" ? (
+									provider.name === "Facebook" ? (
+										<FacebookIcon />
+									) : provider.name === "Instagram" ? (
+										<InstagramIcon />
+									) : provider.name === "Google" ? (
 										<GoogleIcon />
 									) : provider.name === "GitHub" ? (
 										<GitHubIcon />
 									) : (
-										<FacebookIcon />
+										<EmailIcon />
 									)
 								}
 								variant="outlined"
