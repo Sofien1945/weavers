792320f6c31c30234711b052956619e0279faeac
unknown <abidisofien1988@gmail.com>
2022-10-29 10:55:01 +0100

    corrected commit #Add_Article02

--- components/Author.jsx
+++ components/Author.jsx
@@ -2,24 +2,24 @@ import Image from "next/image";
 
 const grpahCMSImageLoader = ({ src }) => src;
 
-const Author = ({ publisher }) => (
+const Author = ({ author }) => (
 	<div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black/20">
 		<div className="absolute left-0 right-0 -top-10">
 			<div className="bg-white rounded-full inline-block p-2 border-2">
 				<Image
 					unoptimized
 					loader={grpahCMSImageLoader}
-					alt={publisher.name}
+					alt={author.name}
 					height="70px"
 					width="70px"
 					className="align-middle"
-					src={publisher.photo.url}
+					src={author.photo.url}
 				/>
 			</div>
 		</div>
 
-		<h3 className="text-white mt-4 mb-4 text-xl font-bold">{publisher.name}</h3>
-		<p className="text-white text-ls">{publisher.bio}</p>
+		<h3 className="text-white mt-4 mb-4 text-xl font-bold">{author.name}</h3>
+		<p className="text-white text-ls">{author.bio}</p>
 	</div>
 );
 
--- pages/home/articles/[slug].js
+++ pages/home/articles/[slug].js
@@ -22,7 +22,7 @@ const PostDetails = ({ post }) => {
 			<div className="container mx-auto px-10 mb-8">
 				<div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
 					<div className="col-span-1 lg:col-span-8">
-						<PostDetail post={post} />
+						{/* <PostDetail post={post} /> */}
 						<Author publisher={post.author} />
 					</div>
 					<div className="col-span-1 lg:col-span-4">
