a98ff9947956fac6c973a80c7e87b53867c9397d
unknown <abidisofien1988@gmail.com>
2022-10-29 10:51:23 +0100

    corrected commit #Add_Article02

--- components/Author.jsx
+++ components/Author.jsx
@@ -2,24 +2,24 @@ import Image from "next/image";
 
 const grpahCMSImageLoader = ({ src }) => src;
 
-const Author = ({ author }) => (
+const Author = ({ publisher }) => (
 	<div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black/20">
 		<div className="absolute left-0 right-0 -top-10">
 			<div className="bg-white rounded-full inline-block p-2 border-2">
 				<Image
 					unoptimized
 					loader={grpahCMSImageLoader}
-					alt={author.name}
+					alt={publisher.name}
 					height="70px"
 					width="70px"
 					className="align-middle"
-					src={author.photo.url}
+					src={publisher.photo.url}
 				/>
 			</div>
 		</div>
 
-		<h3 className="text-white mt-4 mb-4 text-xl font-bold">{author.name}</h3>
-		<p className="text-white text-ls">{author.bio}</p>
+		<h3 className="text-white mt-4 mb-4 text-xl font-bold">{publisher.name}</h3>
+		<p className="text-white text-ls">{publisher.bio}</p>
 	</div>
 );
 
